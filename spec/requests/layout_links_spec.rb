44e542eba37cd5a0ff69399c7271c227cce87f00
sidra077 <sidra077@gmail.com>
2011-04-11 15:06:51 -0400

    Polished up the routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -23,6 +23,11 @@ it "should have a Help page at '/help'" do
   response.should have_selector('title', :content => "Help")
 end
 
+it "should have a signup page at '/signup'" do
+  get '/signup'
+  response.should have_selector('title', :content => "Sign up")
+end
+
 end
 
 #describe "LayoutLinks" do
